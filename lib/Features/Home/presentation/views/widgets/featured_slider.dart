2a77de1dbf09f859c500ec99908052e42916e177
Ahmed_Elgendy <1223048@dtu.edu.eg>
2026-01-10 23:55:43 +0200

    edit

--- lib/Features/Home/presentation/views/widgets/featured_slider.dart
+++ lib/Features/Home/presentation/views/widgets/featured_slider.dart
@@ -1,5 +1,5 @@
-import 'package:bookly_app/Features/Home/presentation/views/widgets/featured_list_view.dart';
 import 'package:bookly_app/Features/Home/presentation/views/widgets/featured_list_view_item.dart';
+import 'package:carousel_slider/carousel_options.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -12,52 +12,56 @@ class HomeSlider extends StatefulWidget {
 }
 
 class _HomeSliderState extends State<HomeSlider> {
-
   int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.sizeOf(context).width;
+
     return SizedBox(
-      height: 200,
+      height: 220,
       child: Stack(
         children: [
           CarouselSlider.builder(
+            itemCount: 10,
+            itemBuilder: (context, index, realIndex) {
+              return FeaturedListViewItem();
+            },
             options: CarouselOptions(
+              height: 220,
               autoPlay: true,
-              autoPlayInterval: Duration(seconds: 4),
-              autoPlayAnimationDuration: Duration(milliseconds: 1600),
-              pauseAutoPlayOnTouch: true,
-              pauseAutoPlayOnManualNavigate: true,
-              pauseAutoPlayInFiniteScroll: false,
-              viewportFraction: 1, // عرض العنصر فقط واخفاء الباقي
-              enlargeCenterPage: true, // تكبير العنصر المركزي
-              height: 200,
+              autoPlayInterval: const Duration(seconds: 4),
+              autoPlayAnimationDuration:
+                  const Duration(milliseconds: 1200),
+
+              /// 👇 عرض أكتر من عنصر
+              viewportFraction: screenWidth < 400 ? 0.75 : 0.6,
+
+              /// 👇 تكبير العنصر اللي في النص
+              enlargeCenterPage: true,
+              enlargeStrategy: CenterPageEnlargeStrategy.scale,
+              enlargeFactor: 0.25,
+
               onPageChanged: (index, reason) {
-                setState(() {
-                  currentIndex = index;
-                });
+                setState(() => currentIndex = index);
               },
             ),
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index, int realIndex) { return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: FeaturedListViewItem(),
-              ); },
           ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: AnimatedSmoothIndicator(
-                activeIndex: currentIndex,
-                count: 10,
-                effect: const WormEffect(
-                  dotHeight: 12,
-                  dotWidth: 12,
-                  spacing: 5,
-                  dotColor: Colors.white,
-                  activeDotColor: Color(0xff004182),
-                ),
+
+          /// Indicator
+          Positioned(
+            bottom: 8,
+            left: 0,
+            right: 0,
+            child: AnimatedSmoothIndicator(
+              activeIndex: currentIndex,
+              count: 10,
+              effect: const WormEffect(
+                dotHeight: 10,
+                dotWidth: 10,
+                spacing: 6,
+                dotColor: Colors.white,
+                activeDotColor: Color(0xff004182),
               ),
             ),
           ),
